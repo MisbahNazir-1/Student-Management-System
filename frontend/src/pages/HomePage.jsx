1ca4c39ef58f22536d37136af279ff1b513ff022
Misbah Nazir <rehmsnmubbashir@gmail.com>
2026-06-14 18:22:37 +0500

    changes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, handleDelete } from 'react';
 import { getAllStudents, deleteStudent } from '../api/studentapi'; 
 import StudentCard from '../component/studentcard';
 import { Alert } from 'react-bootstrap';
