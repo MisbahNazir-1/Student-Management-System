19e379ae5da4a7e3f4bcdce449a2b21ffb3de316
Misbah Nazir <rehmsnmubbashir@gmail.com>
2026-06-14 16:37:48 +0500

    some changes in frontend

--- frontend/src/api/studentapi.jsx
+++ frontend/src/api/studentapi.jsx
@@ -8,15 +8,15 @@ const api = axios.create({
 })
 
 //creating a function that is getting all students API
-export const getAllStudents = async() => (await api.get('/students/showstudents')).data;
+export const getAllStudents = async() => ( await api.get('/api/students/showstudents')).data;
 
-export const getStudentsById = async(id) => (await api.get(`/students/student/${id}`)).data
+export const getStudentsById = async(id) => (await api.get(`/api/students/student/${id}`)).data
 
 
-export const addStudent = async(data) => (await api.post(`/students/addstudent`, data)).data
+export const addStudent = async(data) => (await api.post(`/api/students/addstudent`, data)).data
 
-export const updateStudent = async(id,data) => (await api.post(`/students/updatestudent/${id}`, data)).data
+export const updateStudent = async(id,data) => (await api.post(`/api/students/updatestudent/${id}`, data)).data
 
-export const deleteStudent = async(id) => (await api.delete(`/students/addstudent/${id}`)).data
+export const deleteStudent = async(id) => (await api.delete(`/api/students/addstudent/${id}`)).data
 
 export default (getAllStudents,getStudentsById,addStudent,updateStudent,deleteStudent)
